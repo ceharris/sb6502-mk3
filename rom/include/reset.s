32bb87f9025e74e346e11c6ad8a89dcc6d925ca0
Carl Harris <ceharris@vt.edu>
2025-02-06 15:08:33 -0500

    improve reuseability of reset.s

--- rom/include/reset.s
+++ rom/include/reset.s
@@ -8,40 +8,40 @@
 ; assembler. It might also work for vasm.
 ;
 
-CONF_REG := $FFD8
-ACIA_CTRL := $FFD0
-ACIA_RESET := $3
-MMU_SLOT0 := $FFC0
-CONF_MMUE := $80
-IPL_VECTOR := $F000
-PIVOT_VECTOR := $F0
+_CONF_REG := $FFD8
+_ACIA_CTRL := $FFD0
+_ACIA_RESET := $3
+_MMU_SLOT0 := $FFC0
+_CONF_MMUE := $80
+_IPL_VECTOR := $F000
+_PIVOT_VECTOR := $F0
 
 pivot_fn:
 		; disable MMU
-                lda CONF_REG
-                and #<~CONF_MMUE
-                sta CONF_REG
+                lda _CONF_REG
+                and #<~_CONF_MMUE
+                sta _CONF_REG
 		; jump back to the IPL program
-                jmp IPL_VECTOR
+                jmp _IPL_VECTOR
 
-PIVOT_FN_LENGTH := *-pivot_fn
+_PIVOT_FN_LENGTH := *-pivot_fn
 
 soft_reset:
 		; quiesce the system
                 sei
                 ; reset the ACIA used for the console
-                lda #ACIA_RESET
-                sta ACIA_CTRL
+                lda #_ACIA_RESET
+                sta _ACIA_CTRL
 		; put bank 0 in slot zero since we will disable MMU
-		stz MMU_SLOT0
+		stz _MMU_SLOT0
 		; copy pivot_fn into the zero page
-                ldx #PIVOT_FN_LENGTH
+                ldx #_PIVOT_FN_LENGTH
                 ldy #0
-@copy:
+copy_pivot:
                 lda pivot_fn,y
-                sta PIVOT_VECTOR,y
+                sta _PIVOT_VECTOR,y
                 iny
                 dex
-                bne @copy
-                jmp PIVOT_VECTOR
+                bne copy_pivot
+                jmp _PIVOT_VECTOR
 
